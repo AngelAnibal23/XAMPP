fc09662f4a9db748f1b4eacb75d0483357d71aa1
Guts <aamamanica@unjbg.edu.pe>
2026-01-26 07:08:25 -0500

    switcihng changes

---
+++ actions/procesar_transaccion.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require_once '../config/database.php';
+require_once '../functions/transacciones.php';
+
+// Verificar que el usuario esté logueado
+if (!isset($_SESSION['usuario_id'])) {
+    header('Location: ../login.php');
+
+}
+
+$usuario_id = $_SESSION['usuario_id'];
+$accion = $_POST['accion'] ?? '';
+
+switch ($accion) {
+    case 'agregar':
+        $categoria_id = $_POST['categoria_id'];
+        $descripcion = $_POST['descripcion'];
+        $monto = $_POST['monto'];
+        $fecha = $_POST['fecha'];
+        
+        agregarTransaccion($usuario_id, $categoria_id, $descripcion, $monto, $fecha);
+        header('Location: ../pages/dashboard.php?msg=agregado');
+        break;
+        
+    case 'eliminar':
+        $id = $_POST['id'];
+        eliminarTransaccion($id, $usuario_id);
+        header('Location: ../pages/dashboard.php?msg=eliminado');
+        break;
+        
+    default:
+        header('Location: ../pages/dashboard.php?msg=error');
+}
+?>
