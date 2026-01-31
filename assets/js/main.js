79a4d4f79f2ca44f25ad866c37f85fcb50cc5947
Guts <aamamanica@unjbg.edu.pe>
2026-01-30 23:44:33 -0500

    logout redirecting route to php archive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+function showForm(formType) {
+            // Ocultar todos los formularios
+            document.querySelectorAll('.form-content').forEach(form => {
+                form.classList.remove('active');
+            });
+            
+            // Desactivar todas las pestañas
+            document.querySelectorAll('.tab').forEach(tab => {
+                tab.classList.remove('active');
+            });
+            
+            // Mostrar el formulario seleccionado
+            document.getElementById(formType).classList.add('active');
+            
+            // Activar la pestaña correspondiente
+            event.target.classList.add('active');
+}
+
+// ========== LOGOUT ==========
+
+function cerrarSesion() {
+    if(confirm('¿Seguro que quieres cerrar sesión?')) {
+        window.location.href = '../auth/logout.php';
+    }
+}
+
 // ========== MODAL ==========
 function abrirModal() {
     document.getElementById('modalTransaccion').style.display = 'block';
