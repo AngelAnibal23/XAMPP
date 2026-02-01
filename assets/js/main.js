1a0946f9e4218100d08c1d2499e69c3542a53875
Guts <aamamanica@unjbg.edu.pe>
2026-01-31 23:52:08 -0500

    vaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,20 @@
 function showForm(formType) {
-            // Ocultar todos los formularios
-            document.querySelectorAll('.form-content').forEach(form => {
-                form.classList.remove('active');
-            });
-            
-            // Desactivar todas las pestañas
-            document.querySelectorAll('.tab').forEach(tab => {
-                tab.classList.remove('active');
-            });
-            
-            // Mostrar el formulario seleccionado
-            document.getElementById(formType).classList.add('active');
-            
-            // Activar la pestaña correspondiente
-            event.target.classList.add('active');
-}
-
-document.querySelectorAll('.form-content').forEach(form => {
+    // Ocultar todos los formularios
+    document.querySelectorAll('.form-content').forEach(form => {
         form.classList.remove('active');
     });
-                
+    
     // Desactivar todas las pestañas
     document.querySelectorAll('.tab').forEach(tab => {
         tab.classList.remove('active');
     });
-                
+    
     // Mostrar el formulario seleccionado
     document.getElementById(formType).classList.add('active');
-                
+    
     // Activar la pestaña correspondiente
-    event.target.classList.add('active');           
-
+    event.target.classList.add('active');
+}
 
 window.addEventListener('pageshow', function(event) {
     // Si la página viene del cache (botón atrás)
